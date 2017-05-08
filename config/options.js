7a1912af82892d81271f2eeb782cb4604e06cb1e
Mohammad Kidwai <akidwaimohammad@gmail.com>
2017-05-08 15:53:38 -0400

    added autominer and node detection

---
+++ config/options.js
@@ -0,0 +1,302 @@
+module.exports = {
+	ethereum: {
+		datadir: {
+			description: 'Data directory for the databases and keystore',
+			value: path.join(process.env.HOME, '.ethereum')
+		},
+		keystore: {
+			description: 'Directory for the keystore',
+			value: path.join(process.env.HOME, '.ethereum', 'keystore')
+		},
+		nousb: {
+			description: 'Disables monitoring for and managine USB hardware wallets',
+			value: false
+		},
+		networkid: {
+			description: 'Network identifier (integer, 1=Frontier, 2=Morden (disused), 3=Ropsten, 4=Rinkeby)',
+			value: 1
+		},
+		testnet: {
+			description: 'Ropsten network: pre-configured proof-of-work test network',
+			value: false
+		},
+		rinkeby: {
+			description: 'Rinkeby network: pre-configured proof-of-authority test network',
+			value: false
+		},
+		dev: {
+			description: 'Developer mode: pre-configured private network with several debugging flags',
+			value: false
+		},
+		syncmode: {
+			description:  'Blockchain sync mode ("fast", "full", or "light")',
+			value: 'full'
+		},
+		ethstats: {
+			description: 'Reporting URL of a ethstats service (nodename:secret@host:port)',
+			value: null
+		},
+		identity: {
+			description: 'Custom node name',
+			value: null
+		},
+		lightserv: {
+			description: 'Maximum percentage of time allowed for serving LES requests (0-90)',
+			value: 0
+		},
+		lightpeers: {
+			description: 'Maximum number of LES client peers',
+			value: 20
+		},
+		lightkdf: {
+			description: 'Reduce key-derivation RAM & CPU usage at some expense of KDF strength',
+			value: false
+		}
+	},
+	ethash: {
+		'ethash.cachesinmem':  {
+			description: 'Number of recent ethash caches to keep in memory (16MB each)',
+			value: 2
+		},
+		'ethash.cachesondisk': {
+			description: 'Number of recent ethash caches to keep on disk (16MB each)',
+			value: 3
+		},
+		'ethash.dagdir': {
+			description: 'Directory to store the ethash mining DAGs',
+			value: path.join(process.env.HOME, '.ethash')
+		},
+		'ethash.dagsinmem': {
+			description: 'Number of recent ethash mining DAGs to keep in memory (1+GB each)',
+			value: 1
+		},
+		'ethash.dagsondisk': {
+			description: 'Number of recent ethash mining DAGs to keep on disk (1+GB each)',
+			value: 2
+		}
+	},
+	peformance: {
+		cache: {
+			description: 'Megabytes of memory allocated to internal caching (min 16MB /database forced)',
+			value: 128,
+		},
+		trie_cache_gens: {
+			description: 'Number of trie node generations to keep in memory',
+			value: 120
+		}
+	},
+	account: {
+		unlock: {
+			description: 'Comma separated list of accunts to unlock',
+			value: null
+		},
+		password: {
+			description: 'Password file to use for non-interactive password input',
+			value: null
+		}
+	},
+	api: {
+	  rpc:             {
+	      description: 'Enable the HTTP-RPC server',
+	      value: true
+	  },
+	  rpcaddr:         {
+	      description: 'HTTP-RPC server listening interface (default: "localhost")',
+	      value: 'localhost'
+	  },
+	  rpcport:         {
+	      description: 'HTTP-RPC server listening port (default: 8545)',
+	      value: 8545
+	  },
+	  rpcapi:          {
+	      description: 'API\'s offered over the HTTP-RPC interface',
+	      value: ['web3','eth','net']
+	  },
+	  ws:                   {
+	      description: 'Enable the WS-RPC server',
+	      value: false
+	  },
+	  wsaddr:          {
+	      description: 'WS-RPC server listening interface (default: "localhost")',
+	      value: 'localhost'
+	  },
+	  wsport:          {
+	      description: 'WS-RPC server listening port (default: 8546)',
+	      value: 8546
+	  },
+	  wsapi:           {
+	      description: 'API\'s offered over the WS-RPC interface',
+	      value: []
+	  },
+	  wsorigins:       {
+	      description: 'Origins from which to accept websockets requests',
+	      value: null
+	  },
+	  ipcdisable:           {
+	      description: 'Disable the IPC-RPC server',
+	      value: false
+	  },
+	  ipcpath:              {
+	      description: 'Filename for IPC socket/pipe within the datadir (explicit paths escape it)',
+	      value: path.join(process.env.HOME, '.ethereum', 'geth.ipc')
+	  },
+	  rpccorsdomain:   {
+	      description: 'List of domains from which to accept cross origin requests (browser enforced)',
+	      value: []
+	  },
+	  jspath:    {
+	      description: 'JavaScript root path for loadScript (default: ".")',
+	      value: '.'
+	  },
+	  exec:            {
+	      description: 'Execute JavaScript statement',
+	      value: null
+	  },
+	  preload:         {
+	      description: 'JavaScript files to preload into the console',
+	      value: []
+		}
+	},
+	networking: {
+	  bootnodes: {
+	    description:'List of enode URLs for P2P discovery bootstrap',
+	    value: []
+	  },
+	  port: {
+	    description:'Network listening port (default: 30303)',
+	    value: 30303
+	  },
+	  maxpeers: {
+	    description: 'Maximum number of network peers (network disabled if set to 0) (default: 25)',
+	    value: 25
+	  },
+	  maxpendpeers: {
+	    description: 'Maximum number of pending connection attempts (defaults used if set to 0) (default: 0)',
+	    value: 0
+	  },
+	  nat: {
+	    description: 'NAT port mapping mechanism (any|none|upnp|pmp|extip:<IP>) (default: "any")',
+	    value: 'any'
+	  },
+	  nodiscover: {
+	    description: 'Disables the peer discovery mechanism (manual peer addition)',
+	    value: false
+	  },
+	  v5disc: {
+	    description: 'Enables the experimental RLPx V5 (Topic Discovery) mechanism',
+	    value: false
+	  },
+	  netrestrict: {
+	    description: 'Restricts network communication to the given IP networks (CIDR masks)',
+	    value: null
+	  },
+	  nodekey: {
+	    description: 'P2P node key file',
+	    value: null
+	  },
+	  nodekeyhex: {
+	    description: 'P2P node key as hex (for testing)',
+	    value: null
+	  }
+	},
+	miner: {
+	  mine: {
+	    description: 'Enable mining',
+	    value: false
+	  },
+	  minerthreads: {
+	    description:       'Number of CPU threads to use for mining (default: 8)',
+	    value: 8
+	  },
+	  etherbase: {
+	    description: 'Public address for block mining rewards (default = first account created) (default: "0")',
+	    value: "0"
+	  },
+	  targetgaslimit: {
+	    description:     'Target gas limit sets the artificial target gas floor for the blocks to mine (default: 4712388)',
+	    value: 4712388
+	  },
+	  gasprice: {
+	    description:  'Minimal gas price to accept for mining a transactions',
+	    value: 20000000000
+	  },
+	  extradata: {
+	    description:          'Block extra data set by the miner (default = client version)',
+	    value: null
+	  }
+	},
+	gaspriceoracle: {
+	  gpoblocks: {
+	    description:       'Number of recent blocks to check for gas prices (default: 10)',
+	    value: 10
+	  },
+	  gpopercentile: {
+	    description:   'Suggested gas price is the given percentile of a set of recent transaction gas prices (default: 50)',
+	    value: 50
+	  }
+	},
+	vm: {  
+	  vmdebug: {
+	    description:   'Record information useful for VM and contract debugging',
+	    value: false
+	  }
+	},
+	debug: {
+	  metrics: {
+	    description: 'Enable metrics collection and reporting',
+	    value: false
+	  },
+	  fakepow: {
+	    description: 'Disables proof-of-work verification',
+	    value: false
+	  },
+	  nocompaction: {
+	    description:'Disables db compaction after import',
+	    value: false
+	  },
+	  verbosity: {
+	    description: 'Logging verbosity: 0=silent, 1=error, 2=warn, 3=info, 4=debug, 5=detail (default: 3)',
+	    value: 3
+	  },
+	  vmodule: {
+	    description: 'Per-module verbosity: list of <pattern>=<level> (e.g. eth/*=5,p2p=4)',
+	    value: []
+	  },
+	  backtrace: {
+	    description: 'Request a stack trace at a specific logging statement (e.g. "block.go:271")',
+	    value: null
+	  },
+	  debug: {
+	    description: 'Prepends log messages with call-site location (file and line number)',
+	    value: false
+	  },
+	  pprof: {
+	    description:                    'Enable the pprof HTTP server',
+	    value: false
+	  },
+	  pprofaddr: {
+	    description:          'pprof HTTP server listening interface (default: "127.0.0.1")',
+	    value: '127.0.0.1'
+	  },
+	  pprofport: {
+	    description:          'pprof HTTP server listening port (default: 6060)',
+	    value: 6060
+	  },
+	  memprofilerate: {
+	    description:     'Turn on memory profiling with the given rate (default: 524288)',
+	    value: 524288
+	  },
+	  blockprofilerate: {
+	    description:   'Turn on block profiling with the given rate (default: 0)',
+	    value: 0
+	  },
+	  cpuprofile: {
+	    description:         'Write CPU profile to the given file',
+	    value: null
+	  },
+	  trace: {
+	    description:              'Write execution trace to the given file',
+	    value: null
+	  }
+	}
+}
--- index.js
+++ index.js
@@ -1,111 +1,316 @@
+/**
+  * geth-js
+  * 
+  * @author Mohammad Kidwai
+  * @license MIT 
+  *
+  */
 module.exports = Geth;
 
-var fs						= require('fs');
-var path 	   				= require('path');
-var cp       				= require('child_process');
-var defaults    			= require('./config/settings/defaults');
-
-/** @constructor  Geth  -  Creates a wrapper instance around go-ethereum 
+var fs			= require('fs');
+var path 	   	= require('path');
+var ps 			= require('ps-node');
+var cp       	= require('child_process');
+var Web3 		= require('web3');
+const defaults  = require('./config/options');
+const Genesis   = require('./config/genesis');
+/** 
+ * Creates a wrapper instance around a geth process 
+ * @constructor
  *
- *	@param {object} [opts]  - An object containing any of the ordinary command-line paraters. 
- *
+ * @param {object} 		options  			- 	Options corresponding to the command-line flags for geth
+ * @param {object} 		options.ethereum 	- 	Ethereum options
+ * @param {object} 		options.ethash 		- 	Ethash options
+ * @param {object} 		options.performance - 	Performance turning options
+ * @param {object}  	options.account 	- 	Account options
+ * @param {object}     	options.api 		- 	API options
+ * @param {object}		options.networking 	- 	Networking options
+ * @param {object}		options.miner  		- 	Miner options
+ * @param {object} 		options.gasprice 	- 	Gas price oracle options
+ * @param {object} 		options.vm 			- 	Virtual machine options
+ * @param {object} 		options.debug 		- 	Logging and debugging options
  *
  */
-function Geth (opts) {
-	this.state = 'stopped';
-	this.settings = opts || defaults;
-	for (var key in defaults) {
-		if (!(key in this.settings)) {
-			this.settings[key] = defaults[key];
-		}
+function Geth (options) {
+	this.options = options || {};
+	for (var type in defaults) {
+		for (var option in defaults[type])
+			this.options[option] = this.options[option] || defaults[type][option].value;	
 	}
 
-	this.newAccount = function(pass) {
-		fs.writeFileSync('/tmp/pass', pass);
-		var result = cp.execSync(`geth --datadir ${this.settings.datadir} --password /tmp/pass account new`).toString('utf-8');
-		return '0x' + result.split('{')[1].split('}')[0];
-		fs.unlinkSync('/tmp/pass');
-	}
-
-	this.listAccounts = function () {
-		return cp.execSync(`geth --datadir ${this.settings.datadir} account list`)
-					   .toString('utf-8')
-					   .split('\n')
-					   .slice(0, -1)
-					   .filter((line) => {return line.startsWith("Account #")})
-					   .map(line => '0x' + line.split('{')[1].split('}')[0]); 
-	}
 	this.start = this.start;
+	this.stop = this.stop;
 	this.init = this.init;
+	this.account = {
+		new: (pass) => {
+			fs.writeFileSync('/tmp/pass', pass);
+			var result = cp.execSync(`geth --datadir ${this.options.datadir} --password /tmp/pass account new`).toString('utf-8');
+			return '0x' + result.split('{')[1].split('}')[0];
+			fs.unlinkSync('/tmp/pass');	
+		},
+		list: () => {
+			return cp.execSync(`geth --datadir ${this.options.datadir} account list`)
+						   .toString('utf-8')
+						   .split('\n')
+						   .slice(0, -1)
+						   .filter((line) => {return line.startsWith("Account #")})
+						   .map(line => '0x' + line.split('{')[1].split('}')[0]); 
+			
+		}
+	}
+
+}
+
+Geth.new = function (options) {
+	var geth = typeof options === 'undefined' ? 
+				new Geth() : new Geth(options); 
+	Geth.nodes.push(geth);
 }
 
 
 
-Geth.prototype.init = function (genesis) {
-		if (typeof genesis === 'string') {
-			try {genesis = require(`./config/genesis/${genesis}`)}
-			catch (err) { console.log(err)}
-		}
-		else if (typeof genesis === 'object') {
-			fs.writeFileSync('/tmp/genesis.json', JSON.stringify(genesis, 2, null));
-			var result = cp.execSync('geth --datadir ' + this.settings.datadir +
-				     ' init /tmp/genesis.json').toString('utf-8');			
-			console.log(result);
-			return true;
-		}
-		else {
-			console.log(`error: invalid type '${typeof genesis}' for genesis`);
-			return false;
-		}
-}
 
 Geth.prototype.start = function () {
 	child =	cp.spawn('geth', 
-			 parseargv(this.settings));
+			 parseOpts(this.options));
+
 
 	child.stderr.on('data', (data) =>{
 		data = data.toString('utf-8');
 		if (data.match(/enode/)) {
-			this.settings.enode = 'enode' + data.split('enode')[1].replace('\n', '');
+			this.options.enode = 'enode' + data.split('enode')[1].replace('\n', '');
 		}
-		if (data.match(/ IPC endpoint opened: /)) 
-			this.settings.ipcpath = data.split(' ')[5]
-
-		if (data.match(/ HTTP endpoint opened: /)) {
-			var host = data.split(' ')[5];
-			this.settings.rpc = true;
-			this.settings.rpcport = host.split(':')[1]
-			this.settings.rpcaddr = host.split(':')[0].split('http://')[1]
+		if (data.match (/HTTP/)) {
+			this.rpc.setProvider(new Web3.providers.HttpProvider('http' + data.split('http')[1].replace('\n', '')));
 		}
-		fs.appendFileSync(this.settings.datadir + '/geth.log', data);
-		this.state = 'running';		
+		if (data.match(/IPC/)) {
+			this.ipc.setProvider(new Web3.providers.IpcProvider(this.options.ipcpath, new net.Socket()));
+			extend(this);	
+		}
+		fs.appendFileSync(path.join(this.options.datadir,'geth.log'), data);
+		this.state = 'running';
 	});
+
 	child.on('exit', (code) => {
-		this.state = 'terminated';
+		delete this.options.enode;
 		delete this.stop;
+		this.state = 'terminated';
 	});
 	child.on('err', (err) => {
 		console.log(`error: ${err}`);
 	});
-	this.stop = () => {
-		 return (child.kill('SIGHUP', {stdio: 'inherit'}))
-	}
-	return child.pid;
+}
+
+Geth.prototype.init = function (genesis) {
+	if (typeof genesis === 'string') 
+		genesis = Genesis[genesis];
+
+	fs.writeFileSync('/tmp/genesis.json', JSON.stringify(genesis, 2, null));
+		var result = cp.execSync('geth --datadir ' + this.options.datadir +
+							     ' init /tmp/genesis.json').toString('utf-8');			
+    console.log(result);
 }
 
 
-function parseargv(opts) {
-	var args = [];
-	for (var key in opts) {
-		if (opts[key] === null) continue;
-		if (String(opts[key]) === 'true') {
-			args.push('--' + key);
-			continue;
-		} else if (String(opts[key]) === 'false')
-			continue;
+Geth.prototype.ipc = new Web3();
+Geth.prototype.rpc = new Web3();
 
-		args.push('--' + key);
-		args.push(opts[key]);
+
+
+
+Geth.prototype.stop = function () {
+	ps.kill(this.pid, function (err, result) {
+		if (!err) {
+			console.log("Stopped geth instance", geth.pid);
+		} else {	
+			console.log(err);
+		}
+	})
+}
+
+
+
+
+
+
+
+
+listNodes().then((nodes) => {
+	nodes.forEach((node) => {
+		node.rpc.setProvider(new Web3.providers.HttpProvider('http://' + node.options.rpcaddr + ':' + node.options.rpcport));
+		node.ipc.setProvider(new Web3.providers.IpcProvider(node.options.ipcpath, new net.Socket()));
+		extend(node);
+	})
+	Geth.nodes = nodes;
+});
+
+function parseOpts (options) {
+	var args = [];
+	for (var option in options) {
+		var value = options[option];
+		if (value === null) continue;
+		if (typeof value === 'object' && typeof value.length !== 'undefined') {
+			if (value.length === 0)
+				continue;
+			value = value.join(',');
+		}
+		if (String(value) === 'false') 
+			continue;
+		else if (String(value) === 'true') {
+			args.push('--' + option);
+			continue;
+		}  else if (option === 'trie_cache_gens') {
+			option = 'trie-cache-gens';
+		}
+		args.push('--' + option);
+		args.push(value);
 	}
 	return args;
+}
+
+
+
+
+
+function listNodes () {
+	return new Promise((resolve, reject) => {
+		ps.lookup("geth", function (err, result) { 
+			if(!err) {
+				var results = result.filter((item)=>{return item.command === "geth"});
+				for (var i = 0 ; i < results.length; i++) {
+					var item = results[i];
+					var argv = item.arguments;
+					delete item.arguments;
+					item.client = item.command;
+					delete item.command;
+					item.options = {};
+					for (var j = 0 ; j < argv.length; j++) {
+						if (argv[j].startsWith('--')) {
+							var arg = argv[j].replace('--', '');
+							item.options[arg] = 
+								j+1 < argv.length && !argv[j+1].startsWith('--') ?
+									item.options[arg] = argv[j+1] :
+									item.options[arg] = true;
+						}
+					}
+					if (item.options.rpc) {
+						item.options.rpcport = item.options.rpcport || 8545;
+						item.options.rpcaddr = item.options.rpcaddr || 'localhost'
+						item.options.rpcpapi = item.options.rpcapi|| 'web3,eth,net'
+						item.options.rpcapi = item.options.rpcapi.split(',');
+					}
+					if (!item.options.port)
+						item.options.port = 30303;
+					if (!item.options.datadir)
+						item.options.datadir = path.join(process.env.HOME, '.ethereum')
+					if (!item.options.ipcdisable) {
+						item.options.ipcpath = item.options.ipcpath || path.join(item.options.datadir, 'geth.ipc')
+					}
+					if (item.options.bootnodes)
+						item.options.bootnodes = item.options.bootnodes.split(',');
+					results[i] = new Geth(item.options);
+					results[i].pid = item.pid;
+					results[i].state = 'running';
+					if (!results[i].options.ipcdisable)
+						extend(results[i]);
+				}
+				resolve(results);
+			} else {
+				reject(err);
+				console.log("Error listing active geth nodes");
+			}
+		});
+		
+	})
+}
+
+function start(spawnargs) {
+	var child = cp.spawn('geth', spawnargs);
+	child.on('data', (data) => {
+		delete this.stop;
+	});
+}
+
+function stop(pid) {
+	ps.kill(pid, function (err, result) {
+		if (!err) {
+			console.log(result);
+		} else {
+			console.log("error stopping process", pid);
+		}
+	});
+}
+
+
+function extend(geth) {
+	geth.ipc._extend({
+				property: 'miner',
+				methods: [
+					new geth.ipc._extend.Method({
+						name: 'start',
+						call: 'miner_start',
+						params: 1,
+						inputFormatter: [parseInt],
+						outputFormatter: (val) => {return String(val)}
+					}),
+					new geth.ipc._extend.Method({
+						name: 'stop',
+						call: 'miner_stop',
+						params: 1,
+						inputFormatter: [parseInt],
+						outputFormatter: (val) => {return String(val)}
+					})
+				]
+			});
+			geth.miner = {
+				start: function (n) {
+					n = n || 8;
+					geth.ipc.miner.start(function (err, result) {
+						if (!err) {
+							console.log(result);
+						} else {
+							console.log(err);
+						}
+					})
+				},
+				stop: function () {
+					geth.ipc.miner.stop(function (err, result) {
+						if (!err) {
+							console.log(result);
+						} else {
+							console.log(err);
+						}
+					})
+				},
+				mine: function (txHash) {
+					return new Promise ((resolve, reject) => {
+						geth.ipc.miner.start(8, function (err, result) {
+							if (err) reject(err);
+							else {
+								var filter = geth.rpc.eth.filter('latest');
+								filter.watch(function (err, block) {
+									if (err) {
+										filter.stopWatching();
+										reject(err);
+									} else {
+										var block = geth.rpc.eth.getTransactionReceipt(txHash,
+											function (err, result) {
+												if (!err) {
+													if (result.blockNumber) {
+														geth.ipc.miner.stop((err, result)=>{
+															if (!err) console.log(result);
+															else console.log(err);
+														})
+														filter.stopWatching();
+														resolve(result);
+													}
+												}
+											})
+									}
+								})
+							}
+						})
+					})
+				}
+			}
 }
